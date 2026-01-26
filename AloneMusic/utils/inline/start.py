bb0ea10056db178df44adcc9855c982ee3e4206b
derdomehmet141-sketch <derdomehmet141@gmail.com>
2026-01-27 00:18:00 +0300

    Update start.py

--- AloneMusic/utils/inline/start.py
+++ AloneMusic/utils/inline/start.py
@@ -1,42 +1,25 @@
-#
-# Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
-#
-# This file is part of < https://github.com/TheAloneTeam/AloneMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-
 from pyrogram.types import InlineKeyboardButton
-
 import config
 from AloneMusic import app
 
 
 def start_panel(_):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
-            ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
-    ]
-    return buttons
+    return [[InlineKeyboardButton(text=_["S_B_4"], url=config.SUPPORT_CHANNEL)]]
 
 
 def private_panel(_):
-    buttons = [
+    return [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
+                text=_["S_B_1"],
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_2"], callback_data="shiv_aarumi"),
-            InlineKeyboardButton(text="💌 ʏᴛ-ᴀᴘɪ", callback_data="bot_info_data"),
+            InlineKeyboardButton(text=_["S_B_7"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_4"], url=f"https://t.me/maviduyuru"),
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+        [
+            InlineKeyboardButton(text=_["S_B_3"], callback_data="open_help"),
+        ],
     ]
-    return buttons
