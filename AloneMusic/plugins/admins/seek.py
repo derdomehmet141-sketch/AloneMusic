885a847a85ee39ab0e433d40a7952cede0256255
derdomehmet141-sketch <derdomehmet141@gmail.com>
2026-01-27 00:25:42 +0300

    Update seek.py

--- AloneMusic/plugins/admins/seek.py
+++ AloneMusic/plugins/admins/seek.py
@@ -19,7 +19,7 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    filters.command(["seek", "cseek", "seekback", "cseekback"])
+    filters.command(["ilerisar", "cseek", "seekback", "cseekback"])
     & filters.group
     & ~BANNED_USERS
 )
