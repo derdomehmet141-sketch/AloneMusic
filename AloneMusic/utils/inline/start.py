f204676aa6a6d360bca6efc48156ee62ffad0e20
derdomehmet141-sketch <derdomehmet141@gmail.com>
2026-02-15 01:16:20 +0300

    Update start.py

--- AloneMusic/utils/inline/start.py
+++ AloneMusic/utils/inline/start.py
@@ -37,7 +37,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-            InlineKeyboardButton(text=_["S_B_7"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_6"], user_id=config.OWNER_ID),
         ],
     ]
     return buttons
