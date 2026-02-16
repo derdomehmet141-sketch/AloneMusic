f49779e61e8e22ffc888a1db4e54f5e5c14e0e39
derdomehmet141-sketch <derdomehmet141@gmail.com>
2026-02-16 11:14:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,86 +1,47 @@
-#
-# Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
-#
-# This file is part of < https://github.com/TheAloneTeam/TheAloneMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TheAloneTeam/TheAloneMusic/blob/master/LICENSE >
-# All rights reserved.
-
 import re
 from os import getenv
-
 from dotenv import load_dotenv
 from pyrogram import filters
 
-load_dotenv()
+load_dotenv()  # .env dosyasını yükle
 
-# Get this value from my.telegram.org/apps
+# Çevresel Değişkenleri Yükle
 API_ID = int(getenv("API_ID", 0))
 API_HASH = getenv("API_HASH")
-
-# Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
-
-# Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600))
-
-# Set this to true if you want post ads automatically
 ADS_MODE = getenv("ADS_MODE", None)
-
-# Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", 0))
-
+LOGGER_ID = int(getenv("LOGGER_ID", 0))  # Logger kanal ID'si
 DEBUG_IGNORE_LOG = True
-# Get this value from  on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 8106551502))
 
-## Fill these variables if you're deploying on heroku.
-# Your heroku app name
+# Diğer çevresel değişkenler
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
-# Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-
-UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO",
-    "https://github.com/TheAloneTeam/AloneMusic",
-)
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/TheAloneTeam/AloneMusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
-GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
-)  # Fill this variable if your upstream repository is private
+GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TheAloneTeam")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TheTeamAlone")
-
-# Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", None))
 
-
-# Get this credentials from https://developer.spotify.com/dashboard
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
 
-
-# Maximum limit for fetching playlist's track from youtube, spotify, apple links.
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
-
-
-# Telegram audio and video file size limit (in bytes)
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
-# Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
 
-
-# Get your pyrogram v2 session from @StringFatherBot on Telegram
+# STRING_SESSION'lar
 STRING1 = getenv("STRING_SESSION", None)
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
 STRING5 = getenv("STRING_SESSION5", None)
 
-
+# Diğer gereksiz değişkenler (varsa)
 BANNED_USERS = filters.user()
 adminlist = {}
 lyrical = {}
@@ -88,15 +49,9 @@ votemode = {}
 autoclean = []
 confirmer = {}
 
-
-START_IMG_URL = getenv(
-    "START_IMG_URL",
-    "https://ibb.co/0jsDgHSj",
-)
-PING_IMG_URL = getenv(
-    "PING_IMG_URL",
-    "https://ibb.co/0jsDgHSj",
-)
+# Görseller
+START_IMG_URL = getenv("START_IMG_URL", "https://ibb.co/0jsDgHSj")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://ibb.co/0jsDgHSj")
 PLAYLIST_IMG_URL = "https://files.catbox.moe/34xlvu.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/34xlvu.jpg"
 TELEGRAM_AUDIO_URL = "https://files.catbox.moe/34xlvu.jpg"
@@ -109,6 +64,7 @@ SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/34xlvu.jpg"
 SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/34xlvu.jpg"
 
 
+# Süreyi saniyeye çevirme fonksiyonu
 def time_to_seconds(time):
     stringt = str(time)
     return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
@@ -116,7 +72,7 @@ def time_to_seconds(time):
 
 DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
 
-
+# URL kontrolü (doğru formatta olduklarından emin olma)
 if SUPPORT_CHANNEL:
     if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
         raise SystemExit(
