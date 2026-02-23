68ac25795a5c6b4552a2490f24394ba467083996
derdomehmet141-sketch <derdomehmet141@gmail.com>
2026-02-23 13:11:50 +0300

    Update queue.py

--- AloneMusic/utils/inline/queue.py
+++ AloneMusic/utils/inline/queue.py
@@ -74,6 +74,6 @@ def queue_back_markup(_, CPLAY):
 
 def aq_markup(_, chat_id):
     buttons = [
-        [InlineKeyboardButton(text="ᴄʟᴏsᴇ", callback_data="close")],
+        [InlineKeyboardButton(text="🍷 Kapat", callback_data="close")],
     ]
     return buttons
